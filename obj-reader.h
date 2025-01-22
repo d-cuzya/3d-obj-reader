c40f4228ea6efbd2746ac3c3119cec2ad25a896e
d-cuzya <Admin@DESKTOP-T6OG5FF>
2025-01-22 12:51:57 +0300

    asdsad

--- obj-reader.h
+++ obj-reader.h
@@ -1,15 +1,26 @@
 #pragma once
 #include <vector>
 
-struct Obj {
+//https://en.wikipedia.org/wiki/Wavefront_.obj_file
 
+struct vertex {
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
+
+struct Obj {
+	std::vector<vertex> vertices;
 };
 
 class ObjReader {
 public:
-	Obj read_obj(const char& file_path) {
+	Obj create_obj(const char& file_path) {
 
 	}
+	bool clear_objs() { return false; };
 private:
 	std::vector<Obj*> obj_list;
 };
