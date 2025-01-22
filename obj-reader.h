b84662a7c879f92a2dd0f30f63f20c594ef2a9d5
d-cuzya <Admin@DESKTOP-T6OG5FF>
2025-01-22 12:55:31 +0300

    asdad

--- obj-reader.h
+++ obj-reader.h
@@ -1,7 +1,27 @@
 #pragma once
 #include <vector>
-
+#include <string>
 //https://en.wikipedia.org/wiki/Wavefront_.obj_file
+
+std::vector<std::string> split_str(std::string str, char target) {
+	std::vector<std::string> tmp;
+	std::string once_str = "";
+	//std::cout << str.length() << ") " << str << std::endl;
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (str[i] != target) {
+			once_str += str[i];
+		}
+		else {
+			tmp.push_back(once_str);
+			once_str = "";
+		}
+	}
+	if (once_str != "") {
+		tmp.push_back(once_str);
+	}
+	return tmp;
+}
 
 struct vertex {
 	float x;
