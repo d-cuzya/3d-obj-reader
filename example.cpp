67c9897307ccb454758bfa508bd3c094eff167ac
d-cuzya <Admin@DESKTOP-T6OG5FF>
2025-01-22 12:33:55 +0300

    Добавьте файлы проекта.

---
+++ example.cpp
@@ -0,0 +1,5 @@
+#include "obj-reader.h"
+
+int main() {
+
+}
---
+++ obj-reader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+
+struct Obj {
+
+};
+
+class ObjReader {
+public:
+	Obj read_obj(const char& file_path) {
+
+	}
+private:
+	std::vector<Obj*> obj_list;
+};
